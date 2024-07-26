322e2d2ae7966e6f52bc3f97378d84d31a5fc867
ynn <350041497@qq.com>
2024-07-26 16:07:32 +0800

    提交说明修改

--- find_best_params.py
+++ find_best_params.py
@@ -6,10 +6,11 @@ def train(df, test_param):
     fixed_params = {
     'learning_rate': 0.1,
     'objective': 'binary',  # 目标函数
-        }
+    }
     x_train, x_test , y_train, y_test =  train_test_split(df.drop(columns = ['label']),df['label'],random_state=42)
-
-    lgb =  LGBMClassifier(**fixed_params)
+    # 创建了一个LGBMClassifier对象，使用固定的参数进行初始化
+    lgb =  LGBMClassifier(**fixed_params)  
+    # GridSearchCV：采用穷举法，对预定义的参数网格中的每一个可能的参数组合尝试训练，以找到性能最优的组合
     gsearch1 = GridSearchCV(lgb,param_grid = test_param, scoring='roc_auc',cv=5,n_jobs=-1)
     gsearch1.fit(x_train,y_train)
     param_columns = list(map(lambda x: 'param_'+x,test_param.keys()))
--- fit_model.py
+++ fit_model.py
@@ -17,17 +17,17 @@ def train(df, params):
         if t % 10 != 0 and t not in (99,98,97,96,95,94,93,92,91,90,0):
 
             continue
-        a = np.percentile(y_prob,t)
+        a = np.percentile(y_prob,t) # 计算阈值
         result['predict'] = result['prob'].apply(lambda x: 1 if x >= a else 0)
-        count = result[result['predict'] == 1].shape[0]
-        count_true = result[(result['predict'] == 1 ) & (result['label'] == 1)].shape[0]
-        recall = count_true/result[result['label']==1].shape[0]
-        accuracy = count_true/count
-        up_rate = accuracy/(result[result['label']==1].shape[0]/result.shape[0])
+        count = result[result['predict'] == 1].shape[0] # 输出人数
+        count_true = result[(result['predict'] == 1 ) & (result['label'] == 1)].shape[0] # 输出中再购人数
+        recall = count_true/result[result['label']==1].shape[0] # 输出中再购率
+        accuracy = count_true/count # 输出中再购率
+        up_rate = accuracy/(result[result['label']==1].shape[0]/result.shape[0]) # 输出提升率
         final_result.append(pd.DataFrame([[100-t,count,count_true,result[result['label']==1].shape[0],
                                         round(recall*100,2),a,round(accuracy,2),up_rate]],
                                         columns=['top','输出人数','输出中再购人数','总再购人数','召回率',
-                                                    '得分','准确率','提升率']))
+                                                    '得分','准确率','提升率'])) 
     return pd.concat(final_result),model    
 
 # 测试
